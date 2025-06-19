50171eafdb9acbcdeb465d44ce3c961f31a06a4d
HeyArnoldo <joao.souza@colegiounion.edu.pe>
2025-06-19 15:51:10 -0500

    init

---
+++ src/main/java/studio/joaosouza/bibleMinigames/BibleMinigames.java
@@ -0,0 +1,17 @@
+package studio.joaosouza.bibleMinigames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BibleMinigames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
